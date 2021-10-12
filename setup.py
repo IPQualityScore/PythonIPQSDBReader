17f13bbfc56a1ebb6a8537c4c191c8489759fb9d
IPQualityScore <50187766+IPQualityScore@users.noreply.github.com>
2021-10-11 20:30:09 -0400

    Setup file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PythonIPQSDBReader",
+    version="0.0.1",
+    author="IPQualityScore",
+    author_email="support@ipqualityscore.com",
+    description="IPQualityScore IP Address Reputation & Proxy Detection DB Reader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IPQualityScore/PythonIPQSDBReader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
