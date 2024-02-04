fbae717190201988e0089fbe5acf8115763e695f
Shea Craft <shea.craft81@gmail.com>
2024-02-04 12:36:19 -0500

    Update example to demonstrate IPv6 usage.

--- example.py
+++ example.py
@@ -1,4 +1,14 @@
 from IPQualityScore.DBReader import DBReader
 
-u = DBReader("IPQualityScore-IP-Reputation-Database-IPv4.ipqs").Fetch("8.8.0.0")
-print(u.IsProxy())
+ipv4_client = DBReader("IPQualityScore-IP-Reputation-Database-IPv4.ipqs")
+
+ipv4_record = ipv4_client.Fetch("8.8.0.0")
+
+print(f"IPv4 Is Proxy: {ipv4_record.IsProxy()}")
+
+
+ipv6_client = DBReader("IPQualityScore-IP-Reputation-Database-IPv6.ipqs")
+
+ipv6_record = ipv6_client.Fetch("2001:4860:4860::8888")
+
+print(f"IPv6 Is Proxy: {ipv6_record.IsProxy()}")
